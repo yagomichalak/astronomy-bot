957317d9a26f08608829c6c373130ad901908db3
Yago Michalak <56281335+yagomichalak@users.noreply.github.com>
2020-06-10 21:14:39 -0300

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,27 @@ async def in_servers():
 
 @client.command()
 async def ping(ctx):
-  await ctx.send(f"**Command successfully tested! Ping: '__{round(client.latency / 100)}__'**")
+  await ctx.send(f"**Command successfully tested! Ping: __{round(client.latency / 100)}__**")
+
+
+@client.command()
+@commands.cooldown(1, 10, type=commands.BucketType.guild)
+async def info(ctx):
+    embed = discord.Embed(title='Artemis Bot', description="__**WHAT IS IT?:**__```Hello, the Artemis Bot is an open source bot based on astronomy, which its only purpose is to portray information about the universe.```", colour=discord.Colour.dark_purple(), url="https://discord.gg/8Bemp2a", timestamp=ctx.message.created_at)
+    embed.add_field(name="📚 __**Topics**__",
+                    value="More than 50 topics to explore.",
+                    inline=False)
+    embed.add_field(name="💻 __**Programmed in**__",
+                    value="The Artemis bot was built in Python, and you can find its GitHub repository [here](https://www.google.com/)",
+                    inline=False)
+    embed.add_field(name="🌎 __**Global ranking**__ ", value="It has a global level ranking based on messages sent.", inline=False)
+    embed.set_footer(text=ctx.guild.name,
+                     icon_url='https://cdn.discordapp.com/attachments/719020754858934294/720294157406568458/universe_hub_server_icon.png')
+    embed.set_thumbnail(
+        url=client.user.avatar_url)
+    embed.set_author(name='DNK#6725', url='https://discord.gg/7DyWxSt',
+                     icon_url='https://cdn.discordapp.com/attachments/719020754858934294/720289112040669284/DNK_icon.png')
+    await ctx.send(embed=embed)
 
 @client.command()
 async def load(ctx, extension: str):
