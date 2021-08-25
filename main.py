09e711d23fa269e3235fb475b08c7ef0e8e3dcb3
= <=>
2021-08-25 00:11:10 -0300

    added eval command

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import discord
 from discord.ext import commands, tasks
 import keep_alive
+import inspect
+import io
+import textwrap
+import traceback
+from contextlib import redirect_stdout
 import os
 from itertools import cycle
 from re import match
@@ -162,6 +167,102 @@ async def invite(ctx):
   '''
   invite = 'https://discord.com/oauth2/authorize?client_id=723699955008798752&permissions=126016&scope=bot'
   await ctx.send(f"Here's my invite:\n{invite}")
+
+
+@client.command(hidden=True)
+@commands.is_owner()
+async def eval(ctx, *, body=None):
+  """ (OWNER) Executes a given command from Python onto Discord.
+  :param body: The body of the command. """
+
+  await ctx.message.delete()
+  if not body:
+    return await ctx.send("**Please, inform the code body!**")
+
+  """Evaluates python code"""
+  env = {
+    'ctx': ctx,
+    'client': client,
+    'channel': ctx.channel,
+    'author': ctx.author,
+    'guild': ctx.guild,
+    'message': ctx.message,
+    'source': inspect.getsource
+  }
+
+  def cleanup_code(content):
+    """Automatically removes code blocks from the code."""
+    # remove ```py\n```
+    if content.startswith('```') and content.endswith('```'):
+      return '\n'.join(content.split('\n')[1:-1])
+
+    # remove `foo`
+    return content.strip('` \n')
+
+  def get_syntax_error(e):
+    if e.text is None:
+      return f'```py\n{e.__class__.__name__}: {e}\n```'
+    return f'```py\n{e.text}{"^":>{e.offset}}\n{e.__class__.__name__}: {e}```'
+
+  env.update(globals())
+
+  body = cleanup_code(body)
+  stdout = io.StringIO()
+  err = out = None
+
+  to_compile = f'async def func():\n{textwrap.indent(body, "  ")}'
+
+  def paginate(text: str):
+    '''Simple generator that paginates text.'''
+    last = 0
+    pages = []
+    for curr in range(0, len(text)):
+      if curr % 1980 == 0:
+        pages.append(text[last:curr])
+        last = curr
+        appd_index = curr
+    if appd_index != len(text)-1:
+      pages.append(text[last:curr])
+    return list(filter(lambda a: a != '', pages))
+
+  try:
+    exec(to_compile, env)
+  except Exception as e:
+    err = await ctx.send(f'```py\n{e.__class__.__name__}: {e}\n```')
+    return await ctx.message.add_reaction('\u2049')
+
+  func = env['func']
+  try:
+    with redirect_stdout(stdout):
+      ret = await func()
+  except Exception as e:
+    value = stdout.getvalue()
+    err = await ctx.send(f'```py\n{value}{traceback.format_exc()}\n```')
+  else:
+    value = stdout.getvalue()
+    if ret is None:
+      if value:
+        try:
+
+          out = await ctx.send(f'```py\n{value}\n```')
+        except:
+          paginated_text = paginate(value)
+          for page in paginated_text:
+            if page == paginated_text[-1]:
+              out = await ctx.send(f'```py\n{page}\n```')
+              break
+            await ctx.send(f'```py\n{page}\n```')
+    else:
+      try:
+        out = await ctx.send(f'```py\n{value}{ret}\n```')
+      except:
+        paginated_text = paginate(f"{value}{ret}")
+        for page in paginated_text:
+          if page == paginated_text[-1]:
+            out = await ctx.send(f'```py\n{page}\n```')
+            break
+          await ctx.send(f'```py\n{page}\n```')
+
 
 @client.command()
 async def help(ctx, cmd: str = None):
