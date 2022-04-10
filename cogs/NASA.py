3f23b5e062639bc68f4cbae507bb823fc2dbc8bb
Yago Michalak <yagomichalak@gmail.com>
2022-04-09 21:42:24 -0300

    DOCUMENTED & TYPEHINED the NASA.py file better

--- cogs/NASA.py
+++ cogs/NASA.py
@@ -6,30 +6,31 @@ import os
 from datetime import datetime
 import aiohttp
 import io
+from typing import Optional
 
 class NASA(commands.Cog):
-	'''
-	A category based on NASA's API
-	'''
+	""" A category based on NASA's API """
 
-	def __init__(self, client) -> None:
+	def __init__(self, client: commands.Bot) -> None:
+		""" Class init method. """
+
 		self.client = client
 		self.token = os.getenv('NASA_API_TOKEN')
 		self.session = aiohttp.ClientSession(loop=client.loop)
 
 	@commands.Cog.listener()
-	async def on_ready(self):
+	async def on_ready(self) -> None:
+		""" Tells when the cog is ready to go. """
+
 		print('NASA cog is online!')
   
-
 	@commands.command()
-	async def apod(self, ctx):
-		'''
-		Gets the Astronomy Picture of the Day (APOD)
-		'''
+	async def apod(self, ctx) -> None:
+		""" Gets the Astronomy Picture of the Day (APOD). """
+
 		try:
 			response = requests.get(f"https://api.nasa.gov/planetary/apod?api_key={self.token}")
-		except requests.HTTPError as exception:
+		except requests.HTTPError:
 			return await ctx.send("**I couldn't do that for some reason, try again later!**")
 		else:
 			data = json.loads(response.text)
@@ -40,7 +41,7 @@ class NASA(commands.Cog):
 				embed.set_image(url=data['url'])
 			if copyright := data['copyright']:
 				embed.set_footer(text=copyright)
-		except Exception as error:
+		except Exception:
 			pass
 
 		try:
@@ -49,21 +50,19 @@ class NASA(commands.Cog):
 			return await ctx.send("**It seems we don't have a picture for today yet!**")
 
 	@commands.command(aliases=['s', 'google'])
-	async def search(self, ctx, *, topic: str = None):
-		'''
-		Searches something on NASA's website.
-		:param topic: The topic to search.
-		'''
+	async def search(self, ctx, *, topic: str = None) -> None:
+		""" Searches something on NASA's website.
+		:param topic: The topic to search. """
 
 		if not topic:
 			return await ctx.send("**Please, inform a topic to search!**")
 		topic = topic.replace(' ', '%20')
+
 		try:
 			response = requests.get(f"https://images-api.nasa.gov/search?q={topic}")
 		except requests.HTTPError as exception:
 			return await ctx.send("I couldn't do that for some reason, try again later!")
 		else:
-			#print(dir(response.text))
 			data = json.loads(response.text)
 			list_data = data['collection']['items']
 			if not list_data:
@@ -71,7 +70,6 @@ class NASA(commands.Cog):
 			try:
 				data = list_data[0]
 				ddata = data['data'][0]
-				#print(data)
 				embed = discord.Embed(title=ddata['title'], description=ddata['description'], color=ctx.author.color, timestamp=datetime.strptime(ddata['date_created'], '%Y-%m-%dT%H:%M:%SZ'))
 				embed.set_image(url=data['links'][0]['href'].replace(' ', '%20'))
 			except Exception:
@@ -81,14 +79,13 @@ class NASA(commands.Cog):
 
 	@commands.command()
 	@commands.cooldown(1, 20, type=commands.BucketType.user)
-	async def earth(self, ctx, lat: float = None, lon: float = None, dim: float = 0.025, date = datetime.utcnow().strftime('%Y-%m-%d')):
-		'''
-		Shows a view of the earth from a given latitude and longitude.
+	async def earth(self, ctx, lat: float = None, lon: float = None, dim: float = 0.025, date = datetime.utcnow().strftime('%Y-%m-%d')) -> None:
+		""" Shows a view of the earth from a given latitude and longitude.
 		:param lat: Latitude.
 		:param lon: Longitude.
 		:param dim: Width and hight in degrees (Default=0.025).
-		:param date: The date (YYYY-MM-DD)(Default=today).
-		'''
+		:param date: The date (YYYY-MM-DD)(Default=today). """
+
 		if not lat:
 			return await ctx.send("**You must inform the latitude!**")
 		if not lon:
@@ -104,15 +101,13 @@ class NASA(commands.Cog):
 			print(error)
 			return await ctx.send("I couldn't do that for some reason, try again later!")
 		else:
-			embed = discord.Embed()
 			await ctx.send("**Here's your view!!**", file=discord.File(io.BytesIO(image), 'earth.png'))
 
 	@commands.command()
 	@commands.cooldown(1, 10, type=commands.BucketType.user)
-	async def mw(self, ctx):
-		'''
-		Gets Mars' weather from the last 7 days.
-		'''
+	async def mw(self, ctx) -> None:
+		""" Gets Mars' weather from the last 7 days. """
+
 		root = "https://api.nasa.gov/insight_weather/?api_key=DEMO_KEY&feedtype=json&ver=1.0"
 
 		try:
@@ -136,18 +131,16 @@ class NASA(commands.Cog):
 
 	@commands.command(aliases=['ep', 'exo', 'xplanet'])
 	@commands.cooldown(1, 10, type=commands.BucketType.user)
-	async def exoplanet(self, ctx, index: int = None):
-		'''
-		Gets some information about an exoplanet, when given an index in the scope of the amount of exoplanets available in the database.
-		:param index: The index of the exoplanet.
-		'''
+	async def exoplanet(self, ctx, index: Optional[int] = None) -> None:
+		""" Gets some information about an exoplanet, when given an index in the scope of the amount of exoplanets available in the database.
+		:param index: The index of the exoplanet. """
+
 		root = 'https://exoplanetarchive.ipac.caltech.edu/cgi-bin/nstedAPI/nph-nstedAPI?table=exoplanets&format=json'
 		async with ctx.typing():
 			async with self.session.get(root) as response:
 				json_data = await response.read()
 				data = json.loads(json_data[537:])
-				lenex = len(data)
-				
+				lenex = len(data)		
 
 		if index is None:
 			return await ctx.send(f"**{ctx.author.mention}, {lenex-1} exoplanets were found in our database, please, provide a number between 0 and {lenex-1}!\nEx: o!exoplanet `74`.**")
@@ -155,13 +148,12 @@ class NASA(commands.Cog):
 		if index < 0 or index > lenex -1:
 			return await ctx.send(f"**{ctx.author.mention}, please, inform a number between 0 and {lenex-1}.**")
 
-
 		data = data[index]
 		embed = discord.Embed(
-		title=f"Exoplanet -> {data['pl_hostname']}",
-		description=f"Showing the exoplanet of index {index} out of {lenex}.",
-		color=ctx.author.color,
-		timestamp=ctx.message.created_at
+			title=f"Exoplanet -> {data['pl_hostname']}",
+			description=f"Showing the exoplanet of index {index} out of {lenex}.",
+			color=ctx.author.color,
+			timestamp=ctx.message.created_at
 		)
 
 		embed.add_field(
@@ -170,12 +162,14 @@ class NASA(commands.Cog):
 			)
 
 		embed.add_field(
-		name="`Stellar Columns`",
-		value=f'''```apache\nDistance (pc): {data['st_dist']}\nOptical Magnitude:** {data['st_optmag']}\nOptical Magnitude Band: {data['st_optband']}\nEffective Temperature (K): {data['st_teff']}\nStellar Mass (solar mass): {data['st_mass']}\nG-band (Gaia) (mag): {data['gaia_gmag']}```''', inline=False
+			name="`Stellar Columns`",
+			value=f'''```apache\nDistance (pc): {data['st_dist']}\nOptical Magnitude:** {data['st_optmag']}\nOptical Magnitude Band: {data['st_optband']}\nEffective Temperature (K): {data['st_teff']}\nStellar Mass (solar mass): {data['st_mass']}\nG-band (Gaia) (mag): {data['gaia_gmag']}```''', inline=False
 		)
 
 		embed.set_footer(text=f"Requested by {ctx.author}", icon_url=ctx.author.avatar_url)
 		await ctx.send(embed=embed)
 
-def setup(client):
+def setup(client: commands.Bot) -> None:
+	""" Cog's setup function. """
+
 	client.add_cog(NASA(client))
